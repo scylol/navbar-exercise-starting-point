eba3265eebec27148f6cf4ba889923cbcc57d13d
Dakota Bryant <bryant.dakota@gmail.com>
2017-06-05 10:38:15 -0500

    added gulp/sass

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass');
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch(['sass/*.sass'], ['sass']);
+    gulp.watch(['*.html', 'scripts/*.js'])
+        .on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src('sass/main.sass')
+        .pipe(sass({
+            style: 'compressed'
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
