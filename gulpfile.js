48e2b46727a42bc28512a67c839b5a5a5a4213dc
Scylol <djd3dobbin@gmail.com>
2017-06-05 12:47:05 -0400

    working on header

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 var gulp = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    sass = require('gulp-sass');
+  browserSync = require('browser-sync').create(),
+  sass = require('gulp-sass');
 
 gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: './'
-    });
+  browserSync.init({
+    server: './'
+  });
 
-    gulp.watch(['sass/*.sass'], ['sass']);
-    gulp.watch(['*.html', 'scripts/*.js'])
+  gulp.watch(['sass/*.sass'], ['sass']);
+  gulp.watch(['*.html', 'scripts/*.js'])
         .on('change', browserSync.reload);
 });
 
 gulp.task('sass', function() {
-    return gulp.src('sass/main.sass')
+  return gulp.src('sass/main.sass')
         .pipe(sass({
-            style: 'compressed'
+          style: 'compressed',
+          errLogToConsole: true
         }))
         .pipe(gulp.dest('./'))
         .pipe(browserSync.stream());
